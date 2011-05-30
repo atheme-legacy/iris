16bad942309effa53da6cebfcc250811e5cafa88
Keith Buck <mr_flea@esper.net>
2011-05-29 23:26:25 -0700

    Replace hg stuff in build with equivelant git stuff.

--- bin/dependencies_b.py
+++ bin/dependencies_b.py
@@ -18,7 +18,7 @@ def check_dependencies():
   check_win32()
   i+=check_json()
   i+=check_java()
-  i+=check_hg()
+  i+=check_git()
   
   print "0 errors, %d warnings." % i
   
@@ -55,18 +55,18 @@ def check_java():
     
   return 0
   
-def check_hg():
-  def hg_warn(specific):
-    warn(specific, "mercurial (hg) is not required, but allows qwebirc to save bandwidth by versioning.", "you can get hg at http://www.selenic.com/mercurial/")
+def check_git():
+  def git_warn(specific):
+    warn(specific, "git is not required, but allows qwebirc to save bandwidth by versioning.", "you can get git at http://git-scm.com/")
     
   try:
-    p = subprocess.Popen(["hg", "id"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(["git", "show", "--oneline", "--quiet"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p.communicate()
-    if p.wait() != 0:
-      hg_warn("something went wrong looking for mercurial.")
+    if p.wait() != 1: # For some reason, git returns exit status 1 when --quiet is used with show.
+      git_warn("something went wrong looking for git.")
       return 1
   except: # ugh
-    hg_warn("couldn't find mercurial.")
+    git_warn("couldn't find git.")
     return 1
     
   return 0
