atheme-legacy/iris

commit 1cc0032e4f0a1a53795893c19e3c61d5f23d57ec

Added possibility to bind to a specific outhoing IP/port if the machine has more than one

iris.conf.example
@@ -30,7 +30,10 @@ syslog_addr:
 # SYSLOG PORT: Sets the port to send syslog datagrams to, if syslog_addr is set.
 syslog_port: 514
 
-
+# Binds to a specific IP and port
+# Use this if your machine has more than one outgoing IP
+#bind_ip: 127.0.0.1
+#bind_port: 0
 
 # BACKEND IRC CONNECTION OPTIONS
 # These options provide the needed information for the backend to connect to

qwebirc/ircclient.py
@@ -225,11 +225,18 @@ def createIRC(*args, **kwargs):
   f = QWebIRCFactory(*args, **kwargs)
   server = config.irc["server"]
   port = config.irc["port"]
+  
+  bind_ip = config.execution["bind_ip"]
+  bind_port = config.execution["bind_port"]
+  bindToAddress = None
+  if bind_ip:
+    bindToAddress = (bind_ip, bind_port)
+  
   if config.irc["ssl"]:
       from twisted.internet.ssl import ClientContextFactory
-      reactor.connectSSL(server, port, f, ClientContextFactory())
+      reactor.connectSSL(server, port, f, ClientContextFactory(), bindAddress=bindToAddress)
   else:
-      reactor.connectTCP(server, port, f)
+      reactor.connectTCP(server, port, f, bindAddress=bindToAddress)
   return f
 
 if __name__ == "__main__":

commit 92b63acfb992286a70de6f8b90ebb5a3dd97c395

Fix broken config options introduced in commit 1cc0032e4f
Commit 1cc0032e4f introduced two new config options: bind_ip and
bind_port. Unfortunately, it makes no sense to bind to a specific port
as only one user will be able to connect at a time (when a second user
tries to use iris, the (host, port) tuple will already be in use).
Additionally, the port was never converted from a str to an int, which
caused an exception if bind_ip was defined. Remove the bind_port option
entirely.

Since the bind_ip option has nothing to do with the actual execution of
the iris daemon and instead deals with connecting to the IRC server,
move it from the execution section to the irc section.

iris.conf.example
@@ -30,13 +30,7 @@ syslog_addr:
 # SYSLOG PORT: Sets the port to send syslog datagrams to, if syslog_addr is set.
 syslog_port: 514
 
-# Binds to a specific IP and port
-# Use this if your machine has more than one outgoing IP
-# Example:
-# bind_ip: 127.0.0.1
-# bind_port: 0
-bind_ip:
-bind_port:
+
 
 # BACKEND IRC CONNECTION OPTIONS
 # These options provide the needed information for the backend to connect to
@@ -52,6 +46,10 @@ port: 6667
 
 # SSL: Use SSL to connect to IRC server or not
 ssl: false
+
+# BIND IP: The (local) IP address to bind to for the IRC server connection.
+# Leave unset to bind to the first applicable interface.
+bind_ip:
 
 # REALNAME: The realname field for clients connecting through the
 # webclient.

qwebirc/ircclient.py
@@ -226,11 +226,10 @@ def createIRC(*args, **kwargs):
   server = config.irc["server"]
   port = config.irc["port"]
   
-  bind_ip = config.execution["bind_ip"]
-  bind_port = config.execution["bind_port"]
+  bind_ip = config.irc["bind_ip"]
   bindToAddress = None
   if bind_ip:
-    bindToAddress = (bind_ip, bind_port)
+    bindToAddress = (bind_ip, 0)
   
   if config.irc["ssl"]:
       from twisted.internet.ssl import ClientContextFactory