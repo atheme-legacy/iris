0fa9ddf8d501a9614a7e4a76cbe2771426760686
megawac <megawac@gmail.com>
2014-04-08 11:18:06 -0400

    Add Cache period option
    Add an option to set the duration of the cache. 30 day default

--- qwebirc/util/caching.py
+++ qwebirc/util/caching.py
@@ -2,10 +2,15 @@ from wsgiref.handlers import format_date_time as format_date
 from datetime import date, timedelta
 from time import mktime
 
+import qwebirc.config as config
+
 '''
    Sets the cache headers for a (static resource) request
 '''
-def cache(request, expires=30, public=True):
+def cache(request, expires=None, public=True):
+    if expires is None:
+        expires = int(config.tuneback["http_cache_period"])
+    
     #set expires header
     expiry = (date.today() + timedelta(expires)).timetuple()
     request.responseHeaders.setRawHeaders("expires" , [format_date(mktime(expiry))])
@@ -17,7 +22,5 @@ def cache(request, expires=30, public=True):
     else:
         cache_control += ", private"
     request.responseHeaders.setRawHeaders("cache-control", [cache_control])
- 
-    print dir(request)
 
     return request
